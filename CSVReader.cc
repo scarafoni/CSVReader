c2512978adce090d561cbf2200eddfc8da251714
Dan Scarafoni <dscarafo@u.rochester.edu>
2013-12-28 00:45:53 -0500

    working copy

--- CSVReader.cc
+++ CSVReader.cc
@@ -29,18 +29,52 @@ int main(int argc, char* argv[])
 	getline(csvFile,line);
 	vector<string> colNames;
 	vector<int> colPos;
-	char* tok;
-	for(tok=strtok(str2arr(line),","); tok; tok=strtok(NULL,","))
+	char* tok1;
+	int pos = 0;
+	for(tok1=strtok(str2arr(line),","); tok1; tok1=strtok(NULL,","))
+	{
 		for(int i=0; i<columns.size(); i++)
-			if(strcmp((columns[i]).c_str(),tok) == 0)
+		{
+			if(strcmp((columns[i]).c_str(),tok1) == 0)
 			{
-				colNames.push_back(tok);
-				colPos.push_back(i);
+				//cout << columns[i] << " " << pos<<"\n";
+				colNames.push_back(tok1);
+				colPos.push_back(pos);
 			}
-	delete tok;
+		}
+		pos++;
+	}
+	delete tok1;
+	//go through line by line, find all elements in select columns
+	vector<string> finTab[colNames.size()];
+	char* tok2;
+	while(getline(csvFile,line))
+	{
+		pos = 0;
+		for(tok2=strtok(str2arr(line),","); tok2; tok2=strtok(NULL,","))
+		{
+			for(int i=0; i<colPos.size(); i++)
+			{
+				if(pos == colPos[i])
+					finTab[i].push_back(tok2);	
+			}
+			pos++;
+		}
+	}
+	delete(tok2);
 
-	for(int i = 0; i < colNames.size(); i++)
-		cout << colNames[i] << "\n";
-	//go through line by line
+	//print results to new file
+	ofstream toWrite("results.txt");
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < finTab[i].size(); j++)
+		{
+			toWrite << finTab[i][j];
+			if(j < finTab[i].size()-1)
+				toWrite << ",";
+		}
+		toWrite <<"\n";
+	}
+	toWrite.close();
 	return 0;
 }
